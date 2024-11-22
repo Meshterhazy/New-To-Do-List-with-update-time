f8ed52c5e73a71664946fbd644a23e237734851e
Давид Мадяр <davidmeshterhazy@MacBook-Air-13-M3.local>
2024-11-23 04:03:45 +0700

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use chrono::Local;
 use std::fs::File;
 use std::io::{self, Write};
 
 fn main() {
     println!("Wellome to To-Do List!");
-    println!("Write the notes for today or yesterday!");
+
+    println!(
+    "Write the number of operation: \n (1) Add notes. \n (2) Remove notes.  \n (3) Edit notes. \n (4) Show all notes. \n (5) Exit."
+    );
 
     let filename = "notes.txt";
     let mut _file = match File::create(filename) {
@@ -14,16 +18,54 @@ fn main() {
         }
     };
 
-    let mut todo_item = String::new();
-    io::stdin()
-        .read_line(&mut todo_item)
-        .expect("New note are failed.");
-    todo_item
-        .trim()
-        .parse::<String>()
-        .expect("File get ready to enter");
+    loop {
+        let mut operations = String::new();
+        io::stdin()
+            .read_line(&mut operations)
+            .expect("Operation sellection error");
+        let operation: u8 = match operations.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number. Try again.");
+                continue;
+            }
+        };
 
-    if let Err(n) = _file.write_all(todo_item.as_bytes()) {
-        eprint!("To-Do-File new item: {}", todo_item);
+        println!("Write the notes.");
+
+        let now = Local::now();
+        let _timelaps = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+        match operation {
+            1 => add_notes(filename),
+            2 => edit_notes(filename),
+            3 => remove_notes(filename),
+            4 => show_notes(filename),
+            5 => {
+                println!("U're exited from notex.txt .");
+                break;
+            }
+            _ => {
+                println!("Invalid operation. Try again.");
+            }
+        }
+        println!("Get choise again. ");
     }
 }
+
+fn add_notes(filename: &str) {
+    let mut new_notes = String::new();
+    io::stdin()
+        .read_line(&mut new_notes)
+        .expect("Failed. Try again.");
+    new_notes
+        .trim()
+        .parse::<String>()
+        .expect("Notes get ready to write.");
+}
+
+fn edit_notes(filename: &str) {}
+
+fn remove_notes(filename: &str) {}
+
+fn show_notes(filename: &str) {}
